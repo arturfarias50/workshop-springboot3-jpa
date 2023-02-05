3c06123e19c52f2621ec399a60d42708d576b2f3
arturfarias50 <arturfarias@yahoo.com.br>
2023-02-05 18:21:34 -0300

    Service layer, component registration

---
+++ src/main/java/com/arturfarias/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.arturfarias.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.arturfarias.course.entities.User;
+import com.arturfarias.course.repositeories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj =  repository.findById(id);
+		return obj.get();
+	}
+
+}
