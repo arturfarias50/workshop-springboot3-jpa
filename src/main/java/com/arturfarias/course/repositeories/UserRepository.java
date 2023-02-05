a6e05c7054742cc001bd49b98f948a99d1455eb4
arturfarias50 <arturfarias@yahoo.com.br>
2023-02-05 17:53:14 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/arturfarias/course/repositeories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.arturfarias.course.repositeories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.arturfarias.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+
+}
