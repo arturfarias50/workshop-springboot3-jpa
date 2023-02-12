5df3b167520de8ee5ca4587974a81a90d4a98b4c
arturfarias50 <arturfarias@yahoo.com.br>
2023-02-12 13:05:08 -0300

    Exception handling - update

--- src/main/java/com/arturfarias/course/services/UserService.java
+++ src/main/java/com/arturfarias/course/services/UserService.java
@@ -4,12 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.arturfarias.course.entities.User;
 import com.arturfarias.course.repositeories.UserRepository;
+import com.arturfarias.course.services.exceptions.DatabaseException;
 import com.arturfarias.course.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -35,13 +39,19 @@ public class UserService {
 		repository.deleteById(id);
 		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
 		}
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
